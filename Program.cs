d1869f9842649b04831dd0034b1be5078a290b0b
ravi-dholariya <200540107103@darshan.ac.in>
2024-03-13 18:25:11 +0530

    Final Done

--- Program.cs
+++ Program.cs
@@ -14,19 +14,21 @@ namespace EntityFramworkTask1
         static void Main(string[] args)
         {
             #region Person
+
+            #region Person Model class object
             //PersonModelClass person = new PersonModelClass()
             //{
             //    Name = "yuvi"
             //};
-            //PassportModelClass passport = new PassportModelClass()
-            //{
-            //    PassportNumber = "aa3dd2",
-            //    ExpiryDate = DateTime.Now,
-            //    Person = person
-            //};
-            //person.Passport = passport;
+            #endregion
+
             //PersonController personController = new PersonController();
+
+            #region AddPerson
             //personController.AddPerson(person);
+            #endregion
+
+            #region UpdatePerson
             //Console.WriteLine("Enter the id to Update");
             //int id = Convert.ToInt32(Console.ReadLine());
             //personController.UpdatePerson(id);
@@ -37,6 +39,9 @@ namespace EntityFramworkTask1
             //    Console.Write($"{person1.Name}");
             //    Console.WriteLine();
             //}
+            #endregion
+
+            #region DeletePerson
             //Console.WriteLine("Enter the id to Delete");
             //int deleteid = Convert.ToInt32(Console.ReadLine());
             //personController.DeletePerson(deleteid);
@@ -48,14 +53,29 @@ namespace EntityFramworkTask1
             //    Console.WriteLine();
             //}
             #endregion
- 
+
+            #endregion
+
             #region Passport
+
+            #region Passport Model class object
             //person = personController.GetPersonByName("kedar");
+            //PassportModelClass passport = new PassportModelClass()
+            //{
+            //    PassportNumber = "aa3dd2",
+            //    ExpiryDate = DateTime.Now,
+            //    Person = person
+            //};
+            //person.Passport = passport;
+            #endregion
 
             //PassportController pc = new PassportController();
 
+            #region AddPassport
             //pc.AddPassport(passport);
+            #endregion
 
+            #region UpdatePassport
             //Console.WriteLine("Enter the id to Update");
             //int updateid = Convert.ToInt32(Console.ReadLine());
             //pc.UpdatePassport(updateid);
@@ -68,7 +88,9 @@ namespace EntityFramworkTask1
             //    Console.Write($"{passport1.PersonId}");
             //    Console.WriteLine();
             //}
+            #endregion
 
+            #region DeletePassport
             //Console.WriteLine("Enter the id to Delete");
             //int deleteid = Convert.ToInt32(Console.ReadLine());
             //pc.DeletePassport(deleteid);
@@ -80,6 +102,8 @@ namespace EntityFramworkTask1
             //    Console.Write($"{passport1.PersonId}");
             //    Console.WriteLine();
             //}
+            #endregion
+
             #endregion
 
             #region PersonWithPassport
@@ -109,26 +133,34 @@ namespace EntityFramworkTask1
             #region Inventory
 
             #region Product
-            ProductModelClass product = new ProductModelClass()
-            {
-                ProductName = "Watch",
-                ProductPrice = 1000
-            };
-            CustomerModelClass customer = new CustomerModelClass()
-            {
-                CustomerName = "raj",
 
-            };
-            ProductController productController = new ProductController();
-            productController.AddProduct(product);
-            var products = productController.GetProducts();
-            foreach (var product1 in products)
-            {
-                Console.Write($"{product1.ProductId} | ");
-                Console.Write($"{product1.ProductName} | ");
-                Console.Write($"{product1.ProductPrice}");
-                Console.WriteLine();
-            }
+            #region Product and Customer Model class object
+            //ProductModelClass product = new ProductModelClass()
+            //{
+            //    ProductName = "Watch",
+            //    ProductPrice = 1000
+            //};
+            //CustomerModelClass customer = new CustomerModelClass()
+            //{
+            //    CustomerName = "raj",
+            //};
+            #endregion
+
+            //ProductController productController = new ProductController();
+
+            #region AddProduct
+            //productController.AddProduct(product);
+            //var products = productController.GetProducts();
+            //foreach (var product1 in products)
+            //{
+            //    Console.Write($"{product1.ProductId} | ");
+            //    Console.Write($"{product1.ProductName} | ");
+            //    Console.Write($"{product1.ProductPrice}");
+            //    Console.WriteLine();
+            //}
+            #endregion
+
+            #region UpdateProduct
             //Console.WriteLine("Enter the id to Update");
             //int id = Convert.ToInt32(Console.ReadLine());
             //var producttemp = productController.GetProductById(id);
@@ -141,11 +173,14 @@ namespace EntityFramworkTask1
             //    Console.Write($"{product1.ProductPrice}");
             //    Console.WriteLine();
             //}
+            #endregion
+
+            #region DeleteProduct
             //Console.WriteLine("Enter the id to Delete");
             //int deleteid = Convert.ToInt32(Console.ReadLine());
             //productController.DeleteProduct(deleteid);
             //products = productController.GetProducts();
-                
+
             //foreach (var product1 in products)
             //{
             //    Console.Write($"{product1.ProductId} | ");
@@ -155,27 +190,41 @@ namespace EntityFramworkTask1
             //}
             #endregion
 
+            #endregion
+
             #region Customer
-            CustomerController customerController = new CustomerController();
-            customerController.AddCustomer(customer);
-            var customers = customerController.GetCustomers();
-            foreach (var customer1 in customers)
-            {
-                Console.Write($"{customer1.CustomerId} | ");
-                Console.Write($"{customer1.CustomerName}");
-                Console.WriteLine();
-            }
-            //Console.WriteLine("Enter the id to Update");
-            //int id1 = Convert.ToInt32(Console.ReadLine());
-            //var customertemp = customerController.GetCustomerById(id1);
-            //customerController.UpdateCustomer(customertemp);
-            //customers = customerController.GetCustomers();
+            //CustomerController customerController = new CustomerController();
+
+            #region AddCustomer
+            //customerController.AddCustomer(customer);
+            //var customers = customerController.GetCustomers();
             //foreach (var customer1 in customers)
             //{
             //    Console.Write($"{customer1.CustomerId} | ");
             //    Console.Write($"{customer1.CustomerName}");
             //    Console.WriteLine();
             //}
+            #endregion
+
+            #region UpdateCustomer
+            //Console.WriteLine("Enter the id to Update");
+            //int id1 = Convert.ToInt32(Console.ReadLine());
+            //var customertemp = customerController.GetCustomerById(id1);
+            //customertemp.CustomerName = "Ravi";
+            //customerController.UpdateCustomer(customertemp);
+            //var customers = customerController.GetCustomers();
+            //Console.WriteLine("Customer Table");
+            //Console.WriteLine(new string('-', 100));
+            //foreach (var customer1 in customers)
+            //{
+            //    Console.Write($"{customer1.CustomerId} | ");
+            //    Console.Write($"{customer1.CustomerName}");
+            //    Console.WriteLine();
+            //    Console.WriteLine(new string('-',100));
+            //}
+            #endregion
+
+            #region DeleteCustomer
             //Console.WriteLine("Enter the id to Delete");
             //int deleteid1 = Convert.ToInt32(Console.ReadLine());
             //customerController.DeleteCustomer(deleteid1);
@@ -188,26 +237,36 @@ namespace EntityFramworkTask1
             //}
             #endregion
 
+            #endregion
+
             #region Order
-            OrderModelClass order = new OrderModelClass()
-            {
-                OrderDate = DateTime.Now,
-                Customer = customer,
-                Products = new List<ProductModelClass>()
-                {
-                    product
-                }
-            };
-              
-            OrderController orderController = new OrderController();
-            orderController.AddOrder(order);
-            var orders = orderController.GetOrders();
-            foreach (var order1 in orders)
-            {
-                Console.Write($"{order1.OrderId} | ");
-                Console.Write($"{order1.OrderDate} | ");
-                Console.WriteLine();
-            }
+
+            #region Order Model class object
+            //OrderModelClass order = new OrderModelClass()
+            //{
+            //    OrderDate = DateTime.Now,
+            //    Customer = customer,
+            //    Products = new List<ProductModelClass>()
+            //    {
+            //        product
+            //    }
+            //};
+            #endregion
+
+            //OrderController orderController = new OrderController();
+
+            #region AddOrder
+            //orderController.AddOrder(order);
+            //var orders = orderController.GetOrders();
+            //foreach (var order1 in orders)
+            //{
+            //    Console.Write($"{order1.OrderId} | ");
+            //    Console.Write($"{order1.OrderDate} | ");
+            //    Console.WriteLine();
+            //}
+            #endregion
+
+            #region UpdateOrder
             //Console.WriteLine("Enter the id to Update");
             //int id2 = Convert.ToInt32(Console.ReadLine());
             //var ordertemp = orderController.GetOrderById(id2);
@@ -220,6 +279,9 @@ namespace EntityFramworkTask1
             //    Console.Write($"{order1.Customer.CustomerName}");
             //    Console.WriteLine();
             //}
+            #endregion
+
+            #region DeleteOrder
             //Console.WriteLine("Enter the id to Delete");
             //int deleteid2 = Convert.ToInt32(Console.ReadLine());
             //orderController.DeleteOrder(deleteid2);
@@ -233,10 +295,14 @@ namespace EntityFramworkTask1
             //}
             #endregion
 
+            #endregion
+
             #region InventoryWithOrder
             using (var db = new InventoryContext())
             {
                 var orderswithproducts = db.Orders.Include(x => x.Customer).Include(p => p.Products).ToList();
+                Console.WriteLine("All Details:");
+                Console.WriteLine(new string('-', 100));
                 foreach (var order1 in orderswithproducts)
                 {
                     Console.Write($"{order1.OrderId} | ");
@@ -248,6 +314,7 @@ namespace EntityFramworkTask1
                         Console.Write($"{product1.ProductPrice} | ");
                     }
                     Console.WriteLine();
+                    Console.WriteLine(new string('-', 100));
                 }
             }
 
